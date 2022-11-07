fb286a1e5567744776c1f91e06dbd8ab15e84353
Saiodgm <85772054+cqbzljt@users.noreply.github.com>
2022-11-07 10:26:06 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -6,15 +6,15 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm792996750-5fc752c0b6f37a2d1626e176',
     'm791824199-46b6d3d47dc5b5a99b004b25',
     'm791824263-dfd0f542fc6a1f9c93aadc9a',
+    'm792996750-5fc752c0b6f37a2d1626e176',
     'm791832798-6a4b04c11815b61881bab177',
     'm791832801-6dbb63f7e41c844e347f0c65',
   ],
 
   // 日志天数
-  CountDays: 45,
+  CountDays: 90,
 
   // 是否显示检测站点的链接
   ShowLink: true,
